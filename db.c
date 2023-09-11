105210387e635d65aea112a567c3cd7d1ad99317
jasondsouza35 <jasonaldrindsouza@gmail.com>
2023-09-10 21:24:58 -0400

    created a 'sql compiler' for the database

--- db.c
+++ db.c
@@ -3,7 +3,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
 typedef struct {
+  StatementType type;
+} Statement;
+
+
+typedef struct { // InputBuffer is a struct that is responsible for managing data inputted on the REPL
   char* buffer;
   size_t buffer_length;
   ssize_t input_length;
@@ -20,9 +42,9 @@ InputBuffer* new_input_buffer() {
 
 void print_prompt() { printf("db > "); }
 
-void read_input(InputBuffer* input_buffer) {
+void read_input(InputBuffer* input_buffer) { // Read lines of input from an input buffer
   ssize_t bytes_read =
-      getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+      getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin); 
 
   if (bytes_read <= 0) {
     printf("Error reading input\n");
@@ -34,22 +56,62 @@ void read_input(InputBuffer* input_buffer) {
   input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-void close_input_buffer(InputBuffer* input_buffer) {
+void close_input_buffer(InputBuffer* input_buffer) { // Frees all allocated memory from an InputBuffer
     free(input_buffer->buffer);
     free(input_buffer);
 }
 
-int main(int argc, char* argv[]) {
-  InputBuffer* input_buffer = new_input_buffer();
-  while (true) {
-    print_prompt();
-    read_input(input_buffer);
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  if (strcmp(input_buffer->buffer, "select") == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
 
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+  switch (statement->type) {
+    case (STATEMENT_INSERT):
+      printf("This is where we would do an insert.\n");
+      break;
+    case (STATEMENT_SELECT):
+      printf("This is where we would do a select.\n");
+      break;
+  }
+}
+
+int main(int argc, char* argv[]) {
+   InputBuffer* input_buffer = new_input_buffer();
+   while (true) { // Main function that is responsibile for the read-execute-print loop in the command line (REPL)
+     print_prompt();
+     read_input(input_buffer);
+
+    if (input_buffer->buffer[0] == '.') {
+      switch (do_meta_command(input_buffer)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("Unrecognized command '%s'\n", input_buffer->buffer);
+          continue;
+      }
+     }
+
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Unrecognized keyword at start of '%s'.\n",
+               input_buffer->buffer);
+        continue;
     }
+
+    execute_statement(&statement);
+    printf("Executed.\n");
   }
 }
