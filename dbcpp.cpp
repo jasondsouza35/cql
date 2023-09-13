e4b315844c61ffe45ad94f9ba84b323897450f91
jasondsouza35 <jasonaldrindsouza@gmail.com>
2023-09-12 22:51:48 -0400

    more updates to cpp code

--- dbcpp.cpp
+++ dbcpp.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 enum MetaCommandResult {
@@ -9,6 +10,7 @@ enum MetaCommandResult {
 
 enum PrepareResult {
     PREPARE_SUCCESS, 
+    PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
 };
 
@@ -17,9 +19,91 @@ enum StatementType {
     STATEMENT_SELECT
 };
 
+enum ExecuteResult {
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+};
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+struct Row {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE]; 
+};
+
 struct Statement {
     StatementType type;
+    Row row_to_insert; //only used by insert statement
 };
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+struct Table {
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+};
+
+void print_row(Row* row) {
+    cout << "(" << row->id << ", " << row->username << ", " << row->email <<")"<< endl;
+}
+
+void serialize_row(Row* source, void* destination) {
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* destination) {
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+void* row_slot(Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void *page = table->pages[page_num];
+    if (page == NULL) {
+        // Allocate memory only when we try to access page
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
+
+Table* new_table() {
+    Table* table = new Table;
+    table->num_rows = 0;
+
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        table->pages[i] = NULL;
+    }
+
+    return table;
+}
+
+void free_table(Table* table) {
+    for (int i = 0; table->pages[i]; i++) {
+        delete[] table->pages[i];
+    }
+    delete[] table->pages;
+    delete table;
+}
 
 struct InputBuffer {
     string buffer;
@@ -38,12 +122,13 @@ void read_input(InputBuffer* input_buffer) {
 
 void close_input_buffer(InputBuffer* input_buffer) {
     // delete(input_buffer->buffer);
-    delete(input_buffer);
+    delete input_buffer;
 }
 
-MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table *table) {
     if (input_buffer->buffer == ".exit") {
         close_input_buffer(input_buffer);
+        free_table(table);
         exit(EXIT_SUCCESS);
     } else {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
@@ -53,33 +138,62 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 PrepareResult prepare_statment(InputBuffer* input_buffer, Statement* statement) {
     if (input_buffer->buffer == "insert") {
         statement->type = STATEMENT_INSERT;
+        
+        istringstream iss(input_buffer->buffer);
+        string instruction;
+        iss >> instruction >> statement->row_to_insert.id >> statement->row_to_insert.username >> statement->row_to_insert.email;
+
+        if (statement->row_to_insert.id == 0 || strlen(statement->row_to_insert.username) == 0 || strlen(statement->row_to_insert.email) == 0) {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
-    } else if (input_buffer->buffer == "select") {
+    } 
+
+    if (input_buffer->buffer == "select") {
         statement->type = STATEMENT_SELECT;
         return PREPARE_SUCCESS;
-    } else {
-        return PREPARE_UNRECOGNIZED_STATEMENT;
-    }
+    } 
+    
+    return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
-void execute_statement(Statement* statement) {
+ExecuteResult execute_insert (Statement* statement, Table* table) {
+    if (table->num_rows >= TABLE_MAX_ROWS) {
+        return EXECUTE_TABLE_FULL;
+    }
+
+    Row* row_to_insert = &(statement->row_to_insert);
+
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+}
+
+ExecuteResult execute_select(Statement* statement, Table* table) {
+    Row row;
+    for (uint32_t i = 0; i < table->num_rows; i++) {
+        deserialize_row(row_slot(table, i), &row);
+        print_row(&row);
+    }
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_statement(Statement* statement, Table *table) {
     switch (statement->type) {
         case (STATEMENT_INSERT):
-            cout << "This is where we would do an insert." << endl;
-            break;
+       	    return execute_insert(statement, table);
         case (STATEMENT_SELECT):
-            cout << "This is where we would do an select." << endl;
-            break;
-    }
+	        return execute_select(statement, table);
+  }
 }
 
 int main() {
+    Table* table = new_table();
     InputBuffer* input_buffer = new_input_buffer();
     while (true) {
         read_input(input_buffer);
         
         if (input_buffer->buffer[0] == '.') {
-            switch (do_meta_command(input_buffer)) {
+            switch (do_meta_command(input_buffer, table)) {
                 case (META_COMMAND_SUCCESS):
                     continue;
                 case (META_COMMAND_UNRECOGNIZED_COMMAND):
@@ -92,12 +206,21 @@ int main() {
         switch (prepare_statment(input_buffer, &statement)) {
             case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_SYNTAX_ERROR):
+                cout << "Syntax error. Could not parse statement." << endl;
+                continue;
             case (PREPARE_UNRECOGNIZED_STATEMENT):
                 cout << "Unrecognized keyword at start of " << input_buffer->buffer << "." << endl;
                 continue;
         }
 
-        execute_statement(&statement);
-        cout << "Executed." << endl;
-    }  
+        switch (execute_statement(&statement, table)) {
+            case (EXECUTE_SUCCESS):
+                cout << "Executed." << endl;
+	            break;
+            case (EXECUTE_TABLE_FULL):
+                cout << "Error: Table full." << endl;
+                break;
+        }  
+    }
 }
